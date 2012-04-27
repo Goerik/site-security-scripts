e911bc05cfc2668cd50dd444a1e9bac149308951
Albert Umerov <albert@veryware.org>
2012-04-27 11:14:53 +0400

    Change initial permission to root:root

--- permissions/configure-site-root.sh
+++ permissions/configure-site-root.sh
@@ -6,7 +6,7 @@ if [ -z $1  ]
 then
    echo 'Please specify FULL path to site root!'
 else
-  chown -R $OWNER:$OWNER $1
+  chown -R root:root $1
   chmod -R 777 $1
   find $1 -type d -exec chmod 775 {} \;
   find $1 -type f -exec chmod 664 {} \;
