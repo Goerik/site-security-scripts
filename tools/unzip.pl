8f6b9fa1f7bcead6dfeb2e4b8e176a23b8e0f539
Albert Umerov <albert@veryware.org>
2012-05-15 08:31:15 +0400

    Script for unzipping archive, that created on Windows

---
+++ tools/unzip.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl 
+
+foreach $z (@ARGV) {
+
+    $d = substr($z, 0, -4);
+
+    $zt = `unzip -d $d $z`;
+
+    @files = ($zt =~ /(?<=inflating: ).*?(?=\s*$)/mg);
+
+    &decode_names(@files)
+
+}
+
+
+
+exit;
+
+
+
+sub decode_names {
+
+    foreach $i (@_) {
+
+        $new_name = 
+
+          `echo -n $i | iconv -f cp1252 -t cp850 | iconv -f cp866`;
+
+        ($i eq $new_name) || rename($i, $new_name);
+
+        print "\textracted: $new_name\n";
+
+    }
+
+}
