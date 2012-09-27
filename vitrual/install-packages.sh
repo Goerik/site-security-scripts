f7a146ba8743b81d5808b1cfa7a25d2ae81603cc
Albert Umerov <albert@veryware.org>
2012-09-27 08:37:19 +0400

    nginx-full

--- vitrual/install-packages.sh
+++ vitrual/install-packages.sh
@@ -10,7 +10,7 @@ aptitude install percona-server-server percona-toolkit percona-xtrabackup percon
 aptitude install apache2-mpm-itk libapache2-mod-rpaf php5 libapache2-mod-php5 php5-mysql php5-snmp php5-gd php5-imagick php5-recode php5-xmlrpc php5-xsl php5-mcrypt php5-curl php-pear php5-imap php5-mysql php-apc
 
 # Установка nginx
-aptitude install nginx-naxsi
+aptitude install nginx-full
 
 # Удаляем модуль php5-suhosin
 aptitude purge php5-suhosin
